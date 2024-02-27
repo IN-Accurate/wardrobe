3dd3cea2a9532db30acdbb98877f9995266a9d24
IN-Accurate <vishalsankar410@gmail.com>
2024-02-28 00:20:01 +0530

    a

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Define the absolute path to the uploads directory
+const uploadsDirectory = path.join(__dirname, 'uploads');
+
+// Set up middleware
+app.use('/uploads', express.static(uploadsDirectory));
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
@@ -17,9 +21,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/');
+    cb(null, uploadsDirectory);
   },
   filename: function (req, file, cb) {
     cb(
@@ -29,10 +34,12 @@ const storage = multer.diskStorage({
   },
 });
 
+// Initialize multer with the storage configuration
 const upload = multer({ storage: storage });
 
+// Define routes
 app.get('/wardrobe', (req, res) => {
-  fs.readdir('./uploads', (err, files) => {
+  fs.readdir(uploadsDirectory, (err, files) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal Server Error' });
@@ -64,6 +71,7 @@ app.post('/upload', (req, res) => {
   });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
